88df2854efcaff1683c6f2ea45b0fd8b13fb9ecc
Vladimir <bazoftrope@MacBook-Air-Vladimir.local>
2026-08-30 19:55:35 +0300

    fix: serve navigation pages from network to fix PWA stale/error on login

--- next.config.js
+++ next.config.js
@@ -3,6 +3,31 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  buildExcludes: [
+    /chunks\/pages\/admin\/.*$/,
+    /chunks\/pages\/login.*$/,
+    /chunks\/pages\/index.*$/,
+    /chunks\/pages\/register.*$/,
+  ],
+  runtimeCaching: [
+    {
+      urlPattern: ({ request }) => request.mode === 'navigate',
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'pages',
+        networkTimeoutSeconds: 10,
+        expiration: { maxEntries: 32, maxAgeSeconds: 3600 },
+      },
+    },
+    {
+      urlPattern: /\/icons\/.*/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'icons',
+        expiration: { maxEntries: 20, maxAgeSeconds: 30 * 24 * 3600 },
+      },
+    },
+  ],
 });
 
 /** @type {import('next').NextConfig} */
